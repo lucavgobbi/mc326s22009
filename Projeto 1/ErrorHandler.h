028ce20617c65fd226c2ec4154a31cea40c9df41
lucavgobbi <lucavgobbi@9a23297c-9cd9-11de-ad6c-711f2d8fbc77>
2009-09-17 23:39:54 +0000

    Versão final do Projeto 2a

--- Projeto 1/ErrorHandler.h
+++
@@ -1,5 +0,0 @@
-/*Faz as verifica��es dos parametros de entrada*/
-int Error_InputParameters(int argc, char * argv[]);
-
-/*Verifica se o arquivo pode ser aberto*/
-int Error_FileOpen(char * filePath);
